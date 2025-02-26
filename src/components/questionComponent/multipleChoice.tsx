23c2a9eae2b0bdced16a48708d3acf0893fafefa
Robbie Booth CS2021 <robbie.booth.2021@uni.strath.ac.uk>
2025-02-26 20:44:49 +0000

    removed TODO comment

--- src/components/questionComponent/multipleChoice.tsx
+++ src/components/questionComponent/multipleChoice.tsx
@@ -21,7 +21,7 @@ export function MultipleChoiceQuestion({question, studentAnswer, questionSubmitt
                     return <div className="flex items-center space-x-2" key={index}>
                         <input
                             type="radio"
-                            name={questionID}//TODO if a question has the same name this could cause issues
+                            name={questionID}
                             value={option.option}
                             checked={selectedOption}
                             onChange={(e) => setStudentAnswer(e.target.value)}
